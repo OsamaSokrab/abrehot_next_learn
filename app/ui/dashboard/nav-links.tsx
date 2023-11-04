69795864fef03f543382f16676ee73b4a6bab25d
OsamaSokrab <osamaethio23@gmail.com>
2023-11-04 11:50:00 +0300

    commited

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,6 +4,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  RectangleStackIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -19,6 +20,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'tasks', href: '/dashboard/tasks', icon: RectangleStackIcon },
 ];
 
 export default function NavLinks() {
